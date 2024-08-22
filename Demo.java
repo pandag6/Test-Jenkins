897951b3beb5dac1c614946a229ae6702839b5f7
Goutam Panda <goutampanda1994@gmail.com>
2024-08-23 01:27:58 +0530

    initial commit
    initial commit

---
+++ Demo.java
@@ -0,0 +1,43 @@
+class Calculator
+{
+    public void playMusic()
+
+    {
+        System.out.println("Music Loading....");
+        System.out.println("Bangla Songs....");
+
+    }
+
+    public String getMeAPen(int cost)
+    {
+
+        if (cost >= 10)
+        {
+            return "Get the Pen";
+        }
+        else
+        {
+        return "Sorry Brother";
+        }
+    }
+}
+
+
+
+
+
+public class demo
+{
+
+
+
+public static void main (String[] args)
+{
+    Calculator calc = new Calculator();
+            calc.playMusic();
+            String r = calc.getMeAPen(9); 
+            System.out.println(r);
+
+}
+
+}
